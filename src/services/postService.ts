ea71b38c035837993e4c5805fe7cb6e79dca99e9
Juan Garcia <juanmgarcia098@gmail.com>
2023-03-01 10:43:18 -0400

    refactor

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -14,7 +14,7 @@ async function getAllPosts(offset: number, limit: number): Promise<AllPosts> {
         });
         return (await res.json()) as AllPosts
     } catch (error) {
-        throw error;
+        throw error
     }
 }
 
@@ -30,59 +30,59 @@ async function getSinglePost(id: number): Promise<Post> {
 }
 
 async function createPost(postData: FormData): Promise<Post> {
-  try {
-      const res = await fetch(`${BASE_URL}`, {
-          method: 'POST',
-          headers: {
-              Authorization: `Bearer ${tokenService.getToken()}`,
-          },
-          body: postData,
-      });
-      return (await res.json()) as Post;
-  } catch (err) {
-      throw new Error('Error creating post');
-  }
+    try {
+        const res = await fetch(`${BASE_URL}`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${tokenService.getToken()}`,
+            },
+            body: postData,
+        });
+        return (await res.json()) as Post
+    } catch (err) {
+        throw new Error('Error creating post')
+    }
 }
 
 async function editPost(editData: FormData): Promise<Post | PostMiscReturn> {
-  try {
-      const res = await fetch(`${BASE_URL}`, {
-          method: 'PUT',
-          headers: {
-              Authorization: `Bearer ${tokenService.getToken()}`,
-          },
-          body: editData,
-      });
-      return (await res.json()) as Post;
-  } catch (err) {
-      const error = {
-          type: 'error',
-          message: "Couldn't edit post",
-      };
-      return error
-  }
+    try {
+        const res = await fetch(`${BASE_URL}`, {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${tokenService.getToken()}`,
+            },
+            body: editData,
+        });
+        return (await res.json()) as Post
+    } catch (err) {
+        const error = {
+            type: 'error',
+            message: "Couldn't edit post",
+        }
+        return error
+    }
 }
 
 async function deletePost(id: number): Promise<PostMiscReturn> {
-  try {
-      const res = await fetch(`${BASE_URL}/${id}`, {
-          method: 'DELETE',
-          headers: {
-              Authorization: `Bearer ${tokenService.getToken()}`,
-          },
-      })
-      return { type: 'success', message: 'Post deleted successfully' }
-  } catch (err) {
-      throw err
-  }
+    try {
+        const res = await fetch(`${BASE_URL}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${tokenService.getToken()}`,
+            },
+        })
+        return { type: 'success', message: 'Post deleted successfully' }
+    } catch (err) {
+        throw err
+    }
 }
 
 
 
-export { 
-  getAllPosts, 
-  getSinglePost,
-  createPost,
-  editPost,
-  deletePost
+export {
+    getAllPosts,
+    getSinglePost,
+    createPost,
+    editPost,
+    deletePost
 }
