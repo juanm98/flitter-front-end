2682c1de4b92c5eacf95d135d7b9ec2bbdc14709
Juan Garcia <juanmgarcia098@gmail.com>
2023-03-01 10:38:26 -0400

    Add logo and stub up navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,33 +1,50 @@
 // npm modules
 import { NavLink } from 'react-router-dom'
-
+import './navbar.css'
+import Logo from '../logo/Logo'
 // types
 import { User } from '../../types/models'
 
 interface NavBarProps {
-  user: User | null;
-  handleLogout: () => void;
+  user: User | null
+  handleLogout: () => void
 }
 
 const NavBar = (props: NavBarProps): JSX.Element => {
   const { user, handleLogout } = props
   
   return (
-    <nav>
-      {user ?
-        <ul>
-          <li>Welcome, {user.name}</li>
-          <li><NavLink to="/profiles">Profiles</NavLink></li>
-          <li><NavLink to="/change-password">Change Password</NavLink></li>
-          <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-        </ul>
-      :
-        <ul>
-          <li><NavLink to="/login">Log In</NavLink></li>
-          <li><NavLink to="/signup">Sign Up</NavLink></li>
-        </ul>
-      }
-    </nav>
+    <nav className="container">
+            <Logo />
+            {user ? (
+                <ul className="navlinks">
+                    {/* <li>Welcome, {user.name}</li> */}
+                    <li>
+                        <NavLink to="/profiles">View Listings</NavLink>
+                    </li>
+                    {/* <li>
+                        <NavLink to="/change-password">Change Password</NavLink>
+                    </li> */}
+                    <li>
+                        <NavLink to="/create-post">Create Post</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="" onClick={handleLogout}>
+                            LOG OUT
+                        </NavLink>
+                    </li>
+                </ul>
+            ) : (
+                <ul className="navlinks">
+                    <li>
+                        <NavLink to="/login">Log In</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/signup">Sign Up</NavLink>
+                    </li>
+                </ul>
+            )}
+        </nav>
   )
 }
 
---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './logo.css'
+function Logo(): JSX.Element {
+    return (
+        <div className="logo">
+            <img src="/logo.png" alt="" />
+        </div>
+    )
+}
+
+export default Logo
